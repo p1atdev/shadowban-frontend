dd055349df1e9f23f69c29a77001427129141391
p1atdev <shu_teiei@outlook.jp>
2022-02-20 23:06:16 +0900

    update gtag

--- src/plugins/analytics.client.ts
+++ src/plugins/analytics.client.ts
@@ -1,17 +1,18 @@
-
-import { defineNuxtPlugin } from '#app';
-import VueTag from "vue-gtag";
+import { defineNuxtPlugin } from "#app"
+import VueTag from "vue-gtag"
 
 // Nuxtプラグインの登録
-export default defineNuxtPlugin(nuxtApp => {
-  // ルーター取得
-  const router = useRouter();
+export default defineNuxtPlugin((nuxtApp) => {
+    // ルーター取得
+    const router = useRouter()
 
-  // 測定設定
-  const config = {
-    id: `G-2TKB8NH5NP`, // GA4の測定IDを指定する
-  };
+    const reuntimeConfig = useRuntimeConfig()
 
-  // Vue登録
-  nuxtApp.vueApp.use(VueTag, { config: config }, router);
-});
+    // 測定設定
+    const config = {
+        id: reuntimeConfig.GTAG, // GA4の測定IDを指定する
+    }
+
+    // Vue登録
+    nuxtApp.vueApp.use(VueTag, { config: config }, router)
+})
